cfd611c4d42e27ccdeeab4a43008a7255c51e040
Vepa Hocanazarov <vefa00055@gmail.com>
2023-04-08 15:09:56 +0300

    Add Componentinin Kodlarının eklenmesi

--- src/Components/Add.js
+++ src/Components/Add.js
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Add = () => {
+  const [query, setQuery] = useState("");
+
+  function onChange(e) {
+    setQuery(e.target.value);
+  }
   return (
-    <div>
+    <div className="add-page">
+      <div className="container">
+        <div className="add-content">
+          <img
+            src="https://www.themoviedb.org/t/p/w1920_and_h600_multi_faces_filter(duotone,00192f,00baff)/iNh3BivHyg5sQRPP1KOkzguEX0H.jpg"
+            alt=""
+          />
 
-        Add
-      
+          <div className="titles">
+            <h1>Hoş Geldiniz</h1>
+            <h2>
+              Milyonlarca film,TV şovu ve keşfedilecek kişi.Şimdi keşfedin
+            </h2>
+
+            {query}
+          </div>
+          <div className="input-wrapper">
+            <input
+              type="text"
+              onChange={onChange}
+              placeholder="Film,dizi,kişi ara..."
+            />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Add
+export default Add;
