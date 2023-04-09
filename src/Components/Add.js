e71ed44f2c58efef4f5194e78a8fc501147fa560
Vepa Hocanazarov <vefa00055@gmail.com>
2023-04-09 12:40:05 +0300

    fetch işlemi yapıldı ve  ResulrCard componenti import edildi

--- src/Components/Add.js
+++ src/Components/Add.js
@@ -1,10 +1,27 @@
 import React, { useState } from "react";
+import ResultCard from "./ResultCard";
 
 const Add = () => {
   const [query, setQuery] = useState("");
 
+  const [results, setResults] = useState([]);
+
   function onChange(e) {
     setQuery(e.target.value);
+
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=7af77ea8a95c434b317b8aba93aea7cb&language=en-US&page=1&include_adult=false&query=${e.target.value}
+      `
+      // `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data.errors) {
+          setResults(data.results);
+        } else {
+          setResults([]);
+        }
+      });
   }
   return (
     <div className="add-page">
@@ -20,16 +37,25 @@ const Add = () => {
             <h2>
               Milyonlarca film,TV şovu ve keşfedilecek kişi.Şimdi keşfedin
             </h2>
-
-            {query}
           </div>
           <div className="input-wrapper">
             <input
               type="text"
+              value={query}
               onChange={onChange}
               placeholder="Film,dizi,kişi ara..."
             />
           </div>
+
+          {results.length > 0 && (
+            <ul className="results">
+              {results.map((movie) => (
+                <li key={movie.id} >
+                <ResultCard movie = {movie} />
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       </div>
     </div>
